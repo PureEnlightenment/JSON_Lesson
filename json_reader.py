f6571499547886bf4502cf3be9e579b8d1888bb0
nlc <geczi92@gmail.com>
2021-05-24 01:15:28 +0300

    test git

--- json_reader.py
+++ json_reader.py
@@ -6,7 +6,7 @@ def json_reader():
     single_user_get = {}
     with open('src/single_user_get.json') as read_file:
         single_user_get = json.load(read_file)
-    print(single_user_get, 'data')  # чтение всего содержимого  вложенного json файла
+    print(single_user_get, 'data')   # чтение всего содержимого  вложенного json файла
 
 
 if __name__ == "__main__":
